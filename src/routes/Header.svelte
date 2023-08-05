f2bc5290366a59abbf06d9d7b99b12453f7f0bcb
William Sandvej Hansen <grusinator@gmail.com>
2023-08-05 12:33:41 +0200

    login page working

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -28,6 +28,12 @@
 			<li aria-current={$page.url.pathname.startsWith('/feed') ? 'page' : undefined}>
 				<a href="/feed">Feed</a>
 			</li>
+			<li aria-current={$page.url.pathname.startsWith('/login') ? 'page' : undefined}>
+				<a href="/login">Login</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/register') ? 'page' : undefined}>
+				<a href="/register">Register</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { users } from '../userStore.js';
+
+	let username = '';
+	let password = '';
+
+	const register = () => {
+		// Check if user already exists
+		let userExists = false;
+		$users.forEach(user => {
+			if (user.username === username) {
+				userExists = true;
+			}
+		});
+
+		if (userExists) {
+			alert('User already exists');
+		} else {
+			// Update the store
+			users.update(existingUsers => [...existingUsers, { username, password }]);
+			alert('Registration successful');
+		}
+	};
+</script>
+
+<h1>Register</h1>
+
+<form on:submit|preventDefault={register}>
+	<label for="username">
+		Username
+		<input id="username" bind:value={username} type="text" required />
+	</label>
+	<label for="password">
+		Password
+		<input id="password" bind:value={password} type="password" required />
+	</label>
+	<button type="submit">Register</button>
+</form>
